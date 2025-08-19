b1da78edf2d939ba3ec9bd83b4bd8f825c055be4
Luca Giorgi <lgiorgi@redhat.com>
2025-08-19 20:49:51 +0200

    Add end to end test for MR async job (#487)
    * feat: Add baseline for Async job
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    * feat: add e2e test for async job
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    * fix: remove unused function
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    * fix: remove unused code from utils
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    
    * change: rework and keep only the test, move other functions to fixtures or utils
    
    * change: fix typo in function name
    
    * fix: add types where missing
    
    * change: use the session scope for dsc and metadata_db_resources
    
    * change: work on pr reviews
    
    * change: use image already present and remove artifacts
    
    * change: use wait_for_condition
    
    * change: add jira number to test
    
    * change: skip older test/example to be removed
    
    ---------
    
    Signed-off-by: lugi0 <lgiorgi@redhat.com>
    Co-authored-by: fege <fmosca@redhat.com>

---
+++ tests/model_registry/async_job/constants.py
@@ -0,0 +1,38 @@
+# Job identification
+ASYNC_UPLOAD_JOB_NAME = "model-sync-async-job"
+ASYNC_UPLOAD_IMAGE = "quay.io/opendatahub/model-registry-job-async-upload:v0.2.22"
+
+# Job labels and annotations
+ASYNC_JOB_LABELS = {
+    "app.kubernetes.io/name": "model-registry-async-job",
+    "app.kubernetes.io/component": "async-job",
+    "app.kubernetes.io/part-of": "model-registry",
+    "component": "model-registry-job",
+    "modelregistry.opendatahub.io/job-type": "async-upload",
+}
+
+ASYNC_JOB_ANNOTATIONS = {
+    "modelregistry.opendatahub.io/description": "Asynchronous job for uploading models to Model Registry and converting them to ModelCar format"  # noqa: E501
+}
+
+# Model sync parameters (from sample YAML)
+MODEL_SYNC_CONFIG = {
+    "MODEL_ID": "1",
+    "MODEL_VERSION_ID": "2",
+    "MODEL_ARTIFACT_ID": "1",
+    "SOURCE_TYPE": "s3",
+    "DESTINATION_TYPE": "oci",
+    "SOURCE_AWS_KEY": "my-model",
+    "DESTINATION_OCI_BASE_IMAGE": "public.ecr.aws/docker/library/busybox:latest",
+    "DESTINATION_OCI_ENABLE_TLS_VERIFY": "false",
+}
+
+# Volume mount paths (from sample YAML)
+VOLUME_MOUNTS = {
+    "SOURCE_CREDS_PATH": "/opt/creds/source",
+    "DEST_CREDS_PATH": "/opt/creds/destination",
+    "DEST_DOCKERCONFIG_PATH": "/opt/creds/destination/.dockerconfigjson",
+}
+
+REPO_NAME = "async-job-test/model-artifact"
+TAG = "latest"
