18e7d49d844ec691f242a18d45f9699e690d098e
Shelton Cyril <sheltoncyril@gmail.com>
2026-07-21 20:23:56 +0100

    fix(ogx): add pragma allowlist for postgresql image digest
    detect-secrets flags sha256 digests as high-entropy strings.
    
    Signed-off-by: Shelton Cyril <scyril@redhat.com>
    Signed-off-by: Shelton Cyril <sheltoncyril@gmail.com>

--- utilities/image_constants.py
+++ utilities/image_constants.py
@@ -6,5 +6,6 @@ class SharedImages:
     """
 
     POSTGRESQL_15: str = (
-        "registry.redhat.io/rhel9/postgresql-15@sha256:90ec347a35ab8a5d530c8d09f5347b13cc71df04f3b994bfa8b1a409b1171d59"
+        "registry.redhat.io/rhel9/postgresql-15"
+        "@sha256:90ec347a35ab8a5d530c8d09f5347b13cc71df04f3b994bfa8b1a409b1171d59"  # pragma: allowlist secret
     )
