876905ce60c35514b2d66f271a67033b7bed4fda
Shelton Cyril <sheltoncyril@gmail.com>
2026-07-21 20:23:24 +0100

    fix(spark): add pragma allowlist for image digest
    detect-secrets flags sha256 digests as high-entropy strings.
    
    Signed-off-by: Shelton Cyril <scyril@redhat.com>
    Signed-off-by: Shelton Cyril <sheltoncyril@gmail.com>

--- tests/spark/image_constants.py
+++ tests/spark/image_constants.py
@@ -3,5 +3,5 @@ class SparkImages:
 
     DATA_PROCESSING: str = (
         "quay.io/opendatahub/data-processing"
-        "@sha256:43d2e56d78f374d18210ef5f75713174bc27a7e05c260a2fb0c8c623ed0f084d"
+        "@sha256:43d2e56d78f374d18210ef5f75713174bc27a7e05c260a2fb0c8c623ed0f084d"  # pragma: allowlist secret
     )
